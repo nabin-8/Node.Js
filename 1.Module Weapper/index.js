358c5a9cd93884227dd3ed0ef5c02d7240c51df2
nabin-8 <nabinacharya740@gmail.com>
2024-07-31 09:36:10 +0545

    module wrapper

---
+++ 1.Module Weapper/index.js
@@ -0,0 +1,10 @@
+// Module wrapper
+
+// (function (exports, require, module, __filename, __dirname) {
+//     console.log("Hello from IIFe");
+// })()
+
+// console.log("Hello Nodejs");
+
+console.log(__filename);
+console.log(__dirname);
